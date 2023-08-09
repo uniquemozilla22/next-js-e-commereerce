c1c65f4f9f5e32389e1591bacf479b3302d4b58d
uniquemozilla22 <uniqgaming26@gmail.com>
2023-08-09 14:01:53 -0400

    Added Backend Express to the application

---
+++ src/backend/server.tsx
@@ -0,0 +1,8 @@
+import express from "express";
+import { Express } from "express";
+
+const app: Express = express();
+
+app.listen(8080, () => {
+  console.log("Server Has Started on the port 8080");
+});
